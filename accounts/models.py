b801b16438164766611364b75c92e80c0a3f7b4d
nima-mojarradi <nima.mojarradi83@gmail.com>
2023-09-15 17:30:48 +0430

    feat:new models added

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-class CustomUser(AbstractBaseUser):
-    username = models.CharField('Username', max_length=100, unique=True),
-    first_name = models.CharField('first_name', max_length=100)
-    last_name = models.CharField('last_name', max_length=100)
+class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     phone_number = models.BigIntegerField(null=True, blank=True)
-    cart_info = models.ForeignKey('Cart',on_delete=models.CASCADE)
 
     def __str__(self):
         return self.username
@@ -15,5 +12,5 @@ class CustomUser(AbstractBaseUser):
 
 
 class Cart(models.Model):
-    user = models.OneToOneField("CustomUser", on_delete=models.CASCADE)
-    cart_number = models.BigIntegerField('cart_no')    
+    user = models.OneToOneField("CustomUser", on_delete=models.CASCADE, unique=True)
+    cart_number = models.CharField(max_length=20,verbose_name='cart_no')
