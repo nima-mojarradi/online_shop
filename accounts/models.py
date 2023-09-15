6af53004e7ca4799c53cab533a4ae659887d15f0
nima-mojarradi <nima.mojarradi83@gmail.com>
2023-09-15 18:04:49 +0430

    feat:model of address added

--- accounts/models.py
+++ accounts/models.py
@@ -5,11 +5,15 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     phone_number = models.BigIntegerField(null=True, blank=True)
+    address = models.ManyToManyField('Address')
+    image = models.ImageField(upload_to='images/accounts',null=True)
 
     def __str__(self):
         return self.username
     
-
+class Address(models.Model):
+    location = models.CharField(max_length=255)
+    #TODO
 
 class Cart(models.Model):
     user = models.OneToOneField("CustomUser", on_delete=models.CASCADE, unique=True)
