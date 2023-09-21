44952f8e5515444ceeb6b00bf5b2340a049485b2
nima-mojarradi <nima.mojarradi83@gmail.com>
2023-09-21 15:00:37 +0430

    feat:showing all products view added

--- products/views.py
+++ products/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView,ListCreateAPIView
+from .serializers import ProductSerializer
+from .models import Products
+from django.db.models import Q
 
-# Create your views here.
+class ShowAllProducts(ListAPIView):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        queryset = Products.objects.all()
+        title = self.request.query_params.get('title')
+        if title is not None:
+            queryset = queryset.filter(title=title)
+        return queryset
+    
+
+
+
+class ProductsSearch(ListAPIView):
+    queryset = Products.objects.all()
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        title = self.request.query_params.get('title', '')
+        brand = self.request.query_params.get('brand', '')
+        category = self.request.query_params.get('category', '')
+        queryset = self.queryset
+        print(self.request.query_params)
+        if title:
+            queryset = queryset.filter(Q(title__icontains=title) | Q(brand__icontains=title))
+        if brand:
+            queryset = queryset.filter(Q(brand__title__icontains=brand))
+        if category:
+            queryset = queryset.filter(Q(category__name__icontains=category))
+        return queryset
+    
